9f520f32ac0fa521e72f0f64d4a3f65b933c39af
kadowaki <kadowaki_h@uuum.jp>
2019-09-19 10:23:07 +0900

    update

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'frontend-generators'
 gem 'jquery-rails' 
 gem 'jquery-ui-rails'
 
+gem 'dotenv-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,10 +1,10 @@
 class InquiryMailer < ApplicationMailer
-    def send_mail(inquiry)
+  def send_mail(inquiry)
     @inquiry = inquiry
     mail(
-        from: 'system@example.com',
-        to:   'manager@example.com',
-        subject: 'お問い合わせ通知'
+      from: 'system@example.com',
+      to:   'manager@example.com',
+      subject: 'お問い合わせ通知'
     )
-    end
+  end
 end
