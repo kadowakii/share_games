75de68233de96f15c2f3c7867fbb6530c14d77f0
kadowaki <kadowaki_h@uuum.jp>
2019-10-06 20:14:06 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   }
   
   devise_scope :user do
-    get "users/:id", :to => "users/registrations#show"
+    get "show", :to => "users/registrations#show"
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
