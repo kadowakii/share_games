b6ea37877670aa54ffd72ab236dde5cd9ac70d51
kadowaki <kadowaki_h@uuum.jp>
2019-10-06 20:10:50 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[twitter facebook],
-         :timeoutable
+         :timeoutable, :omniauthable, 
+         omniauth_providers: %i[twitter facebook]
 
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
