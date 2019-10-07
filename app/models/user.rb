600bfddd3ee0d82cb80979deecdc775fefe67316
kadowaki <kadowaki_h@uuum.jp>
2019-10-07 23:24:31 +0900

    relation between user and micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
