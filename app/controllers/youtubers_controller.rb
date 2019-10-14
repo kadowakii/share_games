98ba25596409336a31937c1645aca17204fefce3
kadowaki <kadowaki_h@uuum.jp>
2019-10-15 08:16:54 +0900

    update

--- app/controllers/youtubers_controller.rb
+++ app/controllers/youtubers_controller.rb
@@ -4,6 +4,7 @@ class YoutubersController < ApplicationController
   end
 
   def show
+    @micropost = current_user.microposts.build if user_signed_in?	
     @youtuber = Youtuber.find_by(id: params[:id])
   end
 end
